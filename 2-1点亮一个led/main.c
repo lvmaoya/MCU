81aa02949409e9bf8dd84972595e2b5097b2324a
孙建祥 <1504734652@qq.com>
2024-04-21 22:18:19 +0800

    init

---
+++ 2-1点亮一个led/main.c
@@ -0,0 +1,52 @@
+#include <REGX52.H>
+#include <INTRINS.H>
+
+
+void Delay500ms()		//@12.000MHz
+{
+	unsigned char i, j, k;
+
+	_nop_();
+	i = 4;
+	j = 205;
+	k = 187;
+	do
+	{
+		do
+		{
+			while (--k);
+		} while (--j);
+	} while (--i);
+}
+
+void main()
+{
+	//иак╦
+	//while(1)
+	//{
+	//	Delay500ms();
+	//	P2=0xFE;
+	//	Delay500ms();
+	//	P2=0xFF;
+	//}
+	//иак╦
+	while(1)
+	{
+		P2=0xFE;
+		Delay500ms();
+		P2=0xFD;
+		Delay500ms();
+		P2=0xFB;
+		Delay500ms();
+		P2=0xF7;
+		Delay500ms();
+		P2=0xEF;
+		Delay500ms();
+		P2=0xDF;
+		Delay500ms();
+		P2=0xBF;
+		Delay500ms();
+		P2=0x7F;
+		Delay500ms();
+	}
+} 
